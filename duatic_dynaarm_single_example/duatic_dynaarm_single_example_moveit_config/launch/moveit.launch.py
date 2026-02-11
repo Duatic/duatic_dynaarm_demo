8d6b4e4ca19e22d184eaf0c1677637ffbdef109a
Lennart Nachtigall <mail@firesurfer.de>
2026-02-11 11:02:59 +0100

    remove moveit servo support

--- duatic_dynaarm_single_example/duatic_dynaarm_single_example_moveit_config/launch/moveit.launch.py
+++ duatic_dynaarm_single_example/duatic_dynaarm_single_example_moveit_config/launch/moveit.launch.py
@@ -144,32 +144,6 @@ def launch_setup(context, *args, **kwargs):
         parameters=[moveit_config_dict],
         arguments=["--ros-args", "--log-level", "info"],
     )
-    acceleration_filter_update_period = {"update_period": 0.01}
-    planning_group_name = {"planning_group_name": "dynaarm"}
-
-    # Get parameters for the Servo node
-    servo_params = {
-        "moveit_servo": ParameterBuilder("duatic_dynaarm_single_example_moveit_config")
-        .yaml("config/dynaarm_servo_config.yaml")
-        .to_dict()
-    }
-
-    moveit_servo_node = Node(
-        package="moveit_servo",
-        executable="servo_node",
-        output="screen",
-        namespace=LaunchConfiguration("namespace"),
-        parameters=[
-            servo_params,
-            acceleration_filter_update_period,
-            planning_group_name,
-            moveit_config.robot_description,
-            moveit_config.robot_description_semantic,
-            moveit_config.robot_description_kinematics,
-            moveit_config.joint_limits,
-            {"use_sim_time": mode_value == "sim"},
-        ],
-    )
 
     # RViz
     rviz_config_file = PathJoinSubstitution(
@@ -191,7 +165,7 @@ def launch_setup(context, *args, **kwargs):
         condition=IfCondition(start_rviz),
     )
 
-    nodes_to_start.extend([rviz_node, move_group_node, moveit_servo_node])
+    nodes_to_start.extend([rviz_node, move_group_node])
 
     return nodes_to_start
 
